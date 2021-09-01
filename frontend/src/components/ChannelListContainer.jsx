fc53cfe52db904f9e3f8212541b396e2ad811fef
umair <sumairn.1997@gmail.com>
2021-09-02 00:08:00 +0500

    Adds Auth component

--- frontend/src/components/ChannelListContainer.jsx
+++ frontend/src/components/ChannelListContainer.jsx
@@ -40,6 +40,22 @@ const ChannelListContainer = () => {
 					List={(listProps) => (
 						<TeamChannelList {...listProps} type="team" />
 					)}
+					Preview={(previewProps) => (
+						<TeamChannelPreview {...previewProps} type="team" />
+					)}
+				/>
+				<ChannelList
+					filters={{}}
+					channelRenderFilterFn={() => {}}
+					List={(listProps) => (
+						<TeamChannelList {...listProps} type="messaging" />
+					)}
+					Preview={(previewProps) => (
+						<TeamChannelPreview
+							{...previewProps}
+							type="messaging"
+						/>
+					)}
 				/>
 			</div>
 		</>
